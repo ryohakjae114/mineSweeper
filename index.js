67bcf6a27dc9ee2d9182d148937d78712271449b
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-08-06 17:59:01 +0900

    初回クリックされた位置は必ず地雷がないこと

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 const cell = [[], [], [], [], [], [], [], [], []];
+let freeCellCount = 0;
+
 addEventListener("load", () => {
   createGrid();
   initCells();
-  placeMines();
+  placeMines(20);
   document.querySelectorAll('td').forEach((td) => {
     td.addEventListener('click', () => {
       freeCell(Number(td.dataset.y), Number(td.dataset.x));
@@ -30,18 +32,20 @@ function createGrid() {
 function initCells() {
   cell.forEach(row => {
     for (let x = 0; x < 9; x++) {
-      row[x] = false;
+      row[x] = {};
+      row[x]['mine'] = false;
+      row[x]['free'] = false;
     }
   });
 }
 
-function placeMines() {
-  for (let i = 0; i < 20; i++) {
+function placeMines(count) {
+  for (let i = 0; i < count; i++) {
     let placeMine = decidePlace();
-    while (cell[placeMine[0]][placeMine[1]]) {
+    while (cell[placeMine[0]][placeMine[1]]['mine']) {
       placeMine = decidePlace();
     }
-    cell[placeMine[0]][placeMine[1]] = true;
+    cell[placeMine[0]][placeMine[1]]['mine'] = true;
     const displayCell = document.querySelector(`[data-y="${placeMine[0]}"][data-x="${placeMine[1]}"]`);
     displayCell.innerText = '地雷';
   }
@@ -58,9 +62,18 @@ function getRandomInt(max) {
 }
 
 function freeCell(y, x) {
-  if (cell[y][x]) {
-    // 地雷だった時の処理
-    gameOver();
+  freeCellCount++;
+  cell[y][x]['free'] = true;
+
+  if (cell[y][x]['mine']) {
+    if (freeCellCount === 1) {
+      cell[y][x]['mine'] = false;
+      document.querySelector(`[data-y="${y}"][data-x="${x}"]`).innerText = 'セーフ';
+      placeMines(1);
+    } else {
+      // 地雷だった時の処理 
+      gameOver();
+    }
   } else {
     // クリックされて地雷じゃなくて、隣接する地雷の数を表示する
     const displayCell = document.querySelector(`[data-y="${y}"][data-x="${x}"]`);
@@ -68,7 +81,7 @@ function freeCell(y, x) {
     for (let neighborY = y - 1; neighborY <= y + 1; neighborY++) {
       let count = 0;
       for (let neighborX = x - 1; neighborX <= x + 1; neighborX++) {
-        if (cell[neighborY] !== undefined && cell[neighborY][neighborX] !== undefined && cell[neighborY][neighborX] === true) {
+        if (cell[neighborY] !== undefined && cell[neighborY][neighborX] !== undefined && cell[neighborY][neighborX]['mine'] === true) {
           neighborMinesCount++;
         }
         count++;
